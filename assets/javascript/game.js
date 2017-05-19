b238f309ee681902380044d751b35797d97408de
Andy Lushman <andylushman@gmail.com>
2017-05-19 15:20:56 -0600

    Created characters object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,37 @@
 //GLOBAL VARIABLES
 //===========================================
-var luke = {
-  "healthPoints": 120,
-  "attackPower": 8,
-  "counterAttackPower": 20,
-};
+let characters = {
+  "luke": {
+    "healthPoints": 120,
+    "attackPower": 8,
+    "counterAttackPower": 20,
+  },
 
-var yoda = {
-  "healthPoints": 100,
-  "attackPower": 12,
-  "counterAttackPower": 25,
-};
+  "yoda": {
+    "healthPoints": 100,
+    "attackPower": 12,
+    "counterAttackPower": 25,
+  },
 
-var darthVader = {
-  "healthPoints": 180,
-  "attackPower": 4,
-  "counterAttackPower": 10,
-};
+  "darthVader": {
+    "healthPoints": 180,
+    "attackPower": 4,
+    "counterAttackPower": 10,
+  },
 
-var darthSidious = {
-  "healthPoints": 150,
-  "attackPower": 7,
-  "counterAttackPower": 15,
+  "darthSidious": {
+    "healthPoints": 150,
+    "attackPower": 7,
+    "counterAttackPower": 15,
+  },
 };
+console.log(characters);
 
 
 //FUNCTIONS
 //===========================================
+
+
 
 
 //MAIN PROCESS
