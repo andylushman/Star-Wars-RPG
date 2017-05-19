79d6516efc53f3f21e38f3416eebfb0de7fd49cc
Andy Lushman <andylushman@gmail.com>
2017-05-19 14:24:24 -0600

    First commit since changing the repo name

---
+++ README.md
@@ -0,0 +1,2 @@
+# week-4-game
+Game for week 4 at University of Denver
---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+//GLOBAL VARIABLES
+//===========================================
+var luke = {
+  "healthPoints": 120,
+  "attackPower": 8,
+  "counterAttackPower": 20,
+};
+
+var yoda = {
+  "healthPoints": 100,
+  "attackPower": 12,
+  "counterAttackPower": 25,
+};
+
+var darthVader = {
+  "healthPoints": 180,
+  "attackPower": 4,
+  "counterAttackPower": 10,
+};
+
+var darthSidious = {
+  "healthPoints": 150,
+  "attackPower": 7,
+  "counterAttackPower": 15,
+};
+
+
+//FUNCTIONS
+//===========================================
+
+
+//MAIN PROCESS
+//===========================================
